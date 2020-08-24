7b7fc6f8a0e31df769a6543d87cb0bbb2358778b
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-24 09:26:32 -0600

    Added initial commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//  alert('Hello World!');
+
+/*
+ *  var can be used to declare variables. This can be used anywhere in the program after it is     declared. Therefore they can easily be used in various places in the program. 
+    However, if a var is declared within a function, this variable will instead only be kept inside
+    the function scope
+    It's considered bad practice to use var, unless you absolutely need to use it. 
+ */
+
+ var myName = 'Fahad';          //
+ myName = 'Fahad the Great';       // This will override the above line, JS will assume this is a string because of the letters I used...
+ 
+ /*
+    We can use the 'let' command to declare a variable
+    - It can only be used within the declared block {} only, including functions. 
+ */
+
+ let myAge = 30;
+
+ /*
+    We can use the 'const' command to declare a variable
+    - It can only be used within the declared block {} only, including functions. 
+ */
+
+let myAge = 30;
+
+
+ 
